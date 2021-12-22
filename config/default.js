993c85c17c58c7cc83d16f31858e9c284d78f9a0
unknown <dinhphi751@gmail.com>
2021-12-22 20:26:36 +0700

    wtf

--- config/default.js
+++ config/default.js
@@ -1,9 +1,10 @@
 module.exports = {
-    dbUri: 'mongodb+srv://truongphi:truongphi23@cluster0.wxbhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+   dbUri: 'mongodb+srv://truongphi:truongphi23@cluster0.wxbhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
     "vnp_TmnCode":"7VESNGRU",
     "vnp_HashSecret":"PDTERHIUNKDFYRMOKVTTOKQLXFMAYFRF",
     "vnp_Url": "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-    "vnp_ReturnUrl": "https://doanbooking.netlify.app",
     "redirec": "https://doanbooking.netlify.app",
+    
+    "vnp_ReturnUrl": "http://apidoan-production-5fa1.up.railway.app/order/vnpay_return",
 }
