09aa2c8874be06540272e0373aecfa31ea2c1034
unknown <dinhphi751@gmail.com>
2021-12-22 19:27:57 +0700

    chang vnpay config

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 module.exports = {
     dbUri: 'mongodb+srv://truongphi:truongphi23@cluster0.wxbhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
-    "vnp_TmnCode":"7VESNGRU",
-    "vnp_HashSecret":"PDTERHIUNKDFYRMOKVTTOKQLXFMAYFRF",
+    "vnp_TmnCode":"ODO5U169",
+    "vnp_HashSecret":"WLDIFVOJZZFLOIHGPBKIAOZUIVGVCEZP",
     "vnp_Url": "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-    "vnp_ReturnUrl": "https://doanbooking.netlify.app/order/vnpay_return",
-    "redirec": "https://doanbooking.netlify.app/",
+    "vnp_ReturnUrl": "/order/vnpay_return",
+    "redirec": "https://doanbooking.netlify.app",
 }
